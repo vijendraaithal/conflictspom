03014e43475f77b41cbf4ed11137973858811bd0
Vijendra Aithal <vijendra.aithal@gmail.com>
2016-12-30 16:01:14 +0530

    Added Car

---
+++ src/test/java/com/compassites/Cars.java
@@ -0,0 +1,28 @@
+package com.compassites;
+
+public class Cars {
+		public static int numberOfEmployees = 50;
+		public static int numberOfContractors = 10;
+		
+		public static void main(String[] args) {
+			int numberOfEmployeesLocal = 100;
+			int numberOfContractorsLocal = 200;
+			
+			/*
+			 * Printing Class Variables
+			 */
+			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+			System.out.println("## of Employees - Global: " + numberOfEmployees);
+			System.out.println("## of Contractors - GLOBAL: " + numberOfContractors);
+			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+			
+			/*
+			 * Printing Local Variables
+			 */
+			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+			System.out.println("No of Employees - Global: " + numberOfEmployeesLocal);
+			System.out.println("No of Contractors - Global: " + numberOfContractorsLocal);
+			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		}
+
+}
