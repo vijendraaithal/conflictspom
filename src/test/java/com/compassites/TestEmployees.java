12837000a6c400c05a965a8b10cc27a240dc97ac
Vijendra Aithal <vijendra.aithal@gmail.com>
2016-12-30 16:00:23 +0530

    Squash from here

--- src/test/java/com/compassites/TestEmployees.java
+++ src/test/java/com/compassites/TestEmployees.java
@@ -2,26 +2,28 @@ package com.compassites;
 
 public class TestEmployees {
 	
-	public static int numberOfEmployees = 5;
+	public static int numberOfEmployees = 50;
 	public static int numberOfContractors = 10;
 	
 	public static void main(String[] args) {
-		int numberOfEmployeesLocal = 10;
-		int numberOfContractorsLocal = 20;
+		int numberOfEmployeesLocal = 100;
+		int numberOfContractorsLocal = 200;
 		
 		/*
 		 * Printing Class Variables
 		 */
-
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		System.out.println("## of Employees - Global: " + numberOfEmployees);
 		System.out.println("## of Contractors - GLOBAL: " + numberOfContractors);
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		
 		/*
 		 * Printing Local Variables
 		 */
-		
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 		System.out.println("No of Employees - Global: " + numberOfEmployeesLocal);
 		System.out.println("No of Contractors - Global: " + numberOfContractorsLocal);
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 	}
 	
 	
